317991b7e1ad7c0726c854affbb24572dde9de72
piccoloFrancesco <32369910+piccolofrancesco@users.noreply.github.com>
2017-10-13 14:37:06 +0200

    Ora il debug visualizza il contenuto di tutte e tre le textbox, e si svuotano quando ci si clicca sopra

--- app/src/main/java/com/example/francesco/speseprova/MainActivity.java
+++ app/src/main/java/com/example/francesco/speseprova/MainActivity.java
@@ -15,22 +15,35 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
     }//onCreate
 
-    /**Cerco di svuotare la textbox*/
-    public void empty (View view){
+    /**Svuota la textbox*/
+    public void emptyImporto (View view){
         EditText importo = (EditText) findViewById(R.id.textImporto);
         importo.setText("");
-    }//empty
+    }//emptyImporto
+
+    /**Svuota la textbox*/
+    public void emptyData (View view){
+        EditText importo = (EditText) findViewById(R.id.textImporto);
+        importo.setText("");
+    }//emptyData
+
+    /**Svuota la textbox*/
+    public void emptyDescrizione (View view){
+        EditText importo = (EditText) findViewById(R.id.textImporto);
+        importo.setText("");
+    }//emptyDescrizione
 
     /**Messaggio che visualizza a schermo l'importo */
     public void saveValue (View view){
         EditText importo = (EditText) findViewById(R.id.textImporto);
+        EditText data = (EditText) findViewById(R.id.textData);
+        EditText desc = (EditText) findViewById(R.id.textType);
         TextView textView  = (TextView) findViewById(R.id.textView);
-        textView.setText(importo.getText());
+        //Debug per vedere se ha preso correttamente i dati
+        textView.setText("importo: "+importo.getText()+"\ndata: "+data.getText()+"\ndecrizione: "+desc.getText());
 
-        /*TO DO: salvare il valore da qualche parte
-        in un file?
-        su un db?kkk
-         */
+        //TO DO: salvare il valore su un file
+
         //(cristian) quando si salva il valore,il box torna come all'inizio
         EditText new_importo = (EditText) findViewById(R.id.textImporto);
         new_importo.setText("importo");
